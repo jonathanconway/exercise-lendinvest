452980977b7a40690778f6d634dbdb58a723a524
Jonathan Conway <jonathan.conway@gmail.com>
2017-08-20 14:06:52 +0300

    feat/app render total available for loaded loans

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { loans } from '../../data/current-loans.json'
 import Loan from '../components/Loan'
+import TotalAvailable from '../components/TotalAvailable'
 
 export default class App extends React.Component {
   render = () => <div data-test="app">
@@ -10,5 +11,6 @@ export default class App extends React.Component {
         <Loan key={index} loan={loan} />
       )
     }
+    <TotalAvailable loans={loans} />
   </div>
 }
--- src/containers/__tests__/App.test.jsx
+++ src/containers/__tests__/App.test.jsx
@@ -3,8 +3,20 @@ import { shallow } from 'enzyme'
 
 import App from '../App'
 
+jest.mock('../../../data/current-loans.json', () => ({
+  loans: [{}, {}]
+}))
+
 describe('<App />', () => {
   it('renders', () => {
     const wrapper = shallow(<App />)
+  })
+
+  it(`renders
+      - one loan component per loan in the data-set
+      - one total available component`, () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Loan').length).toEqual(2)
+    expect(wrapper.find('TotalAvailable').length).toEqual(1)
   })
 })
