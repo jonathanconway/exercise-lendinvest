f1acaed08c88f58471582faad66468179b99558c
Jonathan Conway <jonathan.conway@gmail.com>
2017-08-20 16:13:16 +0300

    feat/app render invest dialog when user clicks to invest in a loan

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,14 +3,27 @@ import React from 'react'
 import { loans } from '../../data/current-loans.json'
 import Loan from '../components/Loan'
 import TotalAvailable from '../components/TotalAvailable'
+import InvestDialog from '../components/InvestDialog'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleLoanClickInvest = loanId =>
+    this.setState({
+      investInLoan: loans.filter(loan => loan.id === loanId)[0]
+    })
+
   render = () => <div data-test="app">
     {
       loans.map((loan, index) =>
-        <Loan key={index} loan={loan} onClickInvest={() => {}} />
+        <Loan key={index} loan={loan} onClickInvest={this.handleLoanClickInvest} />
       )
     }
     <TotalAvailable loans={loans} />
+
+    {this.state.investInLoan ? <InvestDialog loan={this.state.investInLoan} /> : null}
   </div>
 }
--- src/containers/__tests__/App.test.jsx
+++ src/containers/__tests__/App.test.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import App from '../App'
 
 jest.mock('../../../data/current-loans.json', () => ({
-  loans: [{}, {}]
+  loans: [{
+      "id": "1",
+      "title": "Voluptate et sed tempora qui quisquam.",
+      "tranche": "A",
+      "available": "11,959",
+      "annualised_return": "8.60",
+      "term_remaining": "864000",
+      "ltv": "48.80",
+      "amount": "85,754"
+    },
+    {
+      "id": "5",
+      "title": "Consectetur ipsam qui magnam minus dolore ut fugit.",
+      "tranche": "B",
+      "available": "31,405",
+      "annualised_return": "7.10",
+      "term_remaining": "1620000",
+      "ltv": "48.80",
+      "amount": "85,754"
+    }]
 }))
 
 describe('<App />', () => {
@@ -18,5 +37,13 @@ describe('<App />', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('Loan').length).toEqual(2)
     expect(wrapper.find('TotalAvailable').length).toEqual(1)
+  })
+
+  it('shows invest dialog, when a loan is clicked, for that loan', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.find('InvestDialog').length).toEqual(0)
+    wrapper.find('Loan button').at(1).simulate('click', '1')
+    expect(wrapper.find('InvestDialog').length).toEqual(1)
+    expect(wrapper.find('InvestDialog').prop('loan')).toEqual(expect.objectContaining({ id: '5' }))
   })
 })
